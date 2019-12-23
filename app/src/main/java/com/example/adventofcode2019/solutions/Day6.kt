149fc7cb38e7641fa0a0b4aa125b19e72c66aae1
Samantha Bull <sam.bull46@gmail.com>
2019-12-23 09:32:02 +0000

    Finished Day 6

--- app/src/main/java/com/example/adventofcode2019/solutions/Day6.kt
+++ app/src/main/java/com/example/adventofcode2019/solutions/Day6.kt
@@ -2,9 +2,10 @@ package com.example.adventofcode2019.solutions
 
 class Day6 : DaySolution() {
 
-    override fun part1Solution(input: String): Int {
-        //parent)child
+    private lateinit var santaOrbit: TreeNode<String>
+    private lateinit var youOrbit: TreeNode<String>
 
+    override fun part1Solution(input: String): Int {
         val orbitMap = HashMap<String, MutableList<String>>()
         input.split("\n").forEach {
             val planets = it.split(")")
@@ -13,27 +14,61 @@ class Day6 : DaySolution() {
         }
         val orbitTree = TreeNode("COM", 0)
 
-        val populatedTree = populateTree(orbitTree, orbitMap)
+        populateTree(orbitTree, orbitMap)
 
         return orbitTree.getDepthSum()
     }
 
     override fun part2Solution(input: String): Int {
-        return 0
+        val orbitMap = HashMap<String, MutableList<String>>()
+        input.split("\n").forEach {
+            val planets = it.split(")")
+            if (orbitMap.containsKey(planets[0])) orbitMap[planets[0]]?.add(planets[1])
+            else orbitMap[planets[0]] = mutableListOf(planets[1])
+        }
+        val orbitTree = TreeNode("COM", 0)
+
+        populateTree(orbitTree, orbitMap)
+
+        val lca = lowestCommonAncestor(santaOrbit, youOrbit) ?: return -1
+
+        return santaOrbit.depth - lca.depth + youOrbit.depth - lca.depth
     }
 
     private fun populateTree(tree: TreeNode<String>, map: Map<String, List<String>>) {
         map[tree.value]?.forEach { tree.addChild(it) }
-        tree.children.forEach { populateTree(it, map) }
+        tree.children.forEach {
+            if (it.value == "SAN") santaOrbit = tree
+            if (it.value == "YOU") youOrbit = tree
+            populateTree(it, map)
+        }
     }
+
+    private fun lowestCommonAncestor(a: TreeNode<String>, b: TreeNode<String>): TreeNode<String>? {
+        val aAncestors = mutableListOf<String>()
+        var node = a
+        while (true) {
+            val bAncestor = node.parent ?: break
+            aAncestors.add(bAncestor.value)
+            node = bAncestor
+        }
+        node = b
+        while (true) {
+            val bAncestor = node.parent ?: break
+            if (aAncestors.contains(bAncestor.value)) return bAncestor
+            node = bAncestor
+        }
+        return null
+    }
+
 }
 
-class TreeNode<T>(val value: T, private val depth: Int) {
+class TreeNode<T>(val value: T, val depth: Int, val parent: TreeNode<T>? = null) {
 
-     val children = mutableListOf<TreeNode<T>>()
+    val children = mutableListOf<TreeNode<T>>()
 
     fun addChild(child: T) {
-         children.add(TreeNode(child, depth + 1))
+        children.add(TreeNode(child, depth + 1, this))
     }
 
     fun getDepthSum(): Int =
@@ -45,4 +80,14 @@ class TreeNode<T>(val value: T, private val depth: Int) {
             sum + depth
         }
 
+    override fun toString() = "{" +
+            "$value " +
+            "${children.map { it.value }}" +
+            "}"
+}
+
+fun <T> MutableList<T>.pop(): T {
+    val value = last()
+    removeAt(size - 1)
+    return value
 }
--- app/src/test/java/com/example/adventofcode2019/solutions/Day6Test.kt
+++ app/src/test/java/com/example/adventofcode2019/solutions/Day6Test.kt
@@ -31,5 +31,18 @@ class Day6Test {
 
     @Test
     fun part2Solution() {
+        assertEquals(4, day6.part2Solution("COM)B\n" +
+                "B)C\n" +
+                "C)D\n" +
+                "D)E\n" +
+                "E)F\n" +
+                "B)G\n" +
+                "G)H\n" +
+                "D)I\n" +
+                "E)J\n" +
+                "J)K\n" +
+                "K)L\n" +
+                "K)YOU\n" +
+                "I)SAN"))
     }
 }
